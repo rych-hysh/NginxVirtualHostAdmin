bdb1d11770f6693d96d4c40a6d8991ee39c06cc9
ubuntu22 <ryo.robolabo@gmail.com>
2023-02-22 14:39:57 +0900

    routing for external component

--- frontInDev/src/app/app-routing.module.ts
+++ frontInDev/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { SitesComponent } from "./components/sites/sites.component";
+import { ExternalComponent } from "./components/external/external.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'sites', component: SitesComponent},
-  
+  {path: 'external', component: ExternalComponent},
 ];
 
 @NgModule({
